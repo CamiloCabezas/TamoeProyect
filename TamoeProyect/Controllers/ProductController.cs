f7f662d6170e858f62ad268ec035c69217b848c7
CamiloCabezas <116015687+CamiloCabezas@users.noreply.github.com>
2024-02-06 13:17:50 -0500

    Agregar archivos de proyecto.

---
+++ TamoeProyect/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TamoeProyect.Models;
+using TamoeProyect.Services;
+
+namespace TamoeProyect.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IRepositorioProducts repositorioProducts;
+
+        public ProductController(IRepositorioProducts repositorioProducts)
+        {
+            this.repositorioProducts = repositorioProducts;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var ListProducts = await repositorioProducts.GetAllProducts();
+            return View(ListProducts);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductViewModel modelo)
+        {
+            if (!ModelState.IsValid)
+            {
+                Console.WriteLine("Esta malo");
+                return View(modelo);
+            }
+
+            await repositorioProducts.PostProduct(modelo);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TamoeProyect/Services/RepositorioProducts.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using TamoeProyect.Models;
+
+namespace TamoeProyect.Services
+{
+    public interface IRepositorioProducts
+    {
+        Task PostProduct(ProductViewModel modelo);
+        Task<IEnumerable<ProductViewModel>> GetAllProducts();
+    }
+    public class RepositorioProducts : IRepositorioProducts
+    {
+        private readonly string connectionString;
+        public RepositorioProducts(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+       public async Task PostProduct(ProductViewModel modelo)
+       {
+           Console.WriteLine("Hola");
+           using var connection = new SqlConnection(connectionString);
+
+           var id = await connection.QuerySingleAsync<int>(@"INSERT INTO Products(Name, Description, Price)
+                                                            VALUES(@Name, @Description, @Price)
+                                                            SELECT SCOPE_IDENTITY();"
+                                                            ,new {modelo.Name, modelo.Description, modelo.Price});
+
+            modelo.Id = id;
+       }
+
+        public async Task<IEnumerable<ProductViewModel>> GetAllProducts() 
+        {
+            Console.WriteLine("Repositorio");
+            using var connection = new SqlConnection(connectionString);
+
+            return await connection.QueryAsync<ProductViewModel>(@"SELECT Name, Description, Price
+                                                        FROM Products");
+
+        }
+
+    }
+}

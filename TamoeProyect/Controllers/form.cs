98f4ba65a7e459b4ab338a919d5750bd728e2eca
CamiloCabezas <116015687+CamiloCabezas@users.noreply.github.com>
2024-02-11 16:09:19 -0500

    Creacion de vista de regitro de usuario

---
+++ TamoeProyect/Controllers/form.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TamoeProyect.Controllers
+{
+    public class form: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TamoeProyect/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TamoeProyect.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo Nombre es obligatorio.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "El campo Email es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El campo Email no tiene un formato válido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El campo Contraseña es obligatorio.")]
+        [StringLength(100, ErrorMessage = "La {0} debe tener al menos {2} caracteres de longitud.", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        public string Role { get; set; } = "User";
+    }
+}
